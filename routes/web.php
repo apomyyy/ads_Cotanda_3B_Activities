f1d2a32497b69267069b65c48de6f3bfcf2bd514
Vismark Cotanda <pomsmanding@gmail.com>
2024-09-05 15:17:34 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,34 @@
 <?php
-
+use App\Model\Student;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/students/create', function() {
+    $student = new Student();
+    $student->first_name = 'Vismark';
+    $student->last_name = 'Cotanda';
+    $student->email = 'pomsmanding@gmail.com';
+    $student->save();
+    return 'Student Created!';
+});
+
+Route::get('/students', function(){
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function(){
+    $student = Student::where('email', 'pomsmanding@gmail.com')->first();
+    $student->email = 'pomsmanding@gmail.com';
+    $student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/students/delete', function (){
+    $student = Student::where('email', 'pomsmanding@gmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
 
 /*
 |--------------------------------------------------------------------------
